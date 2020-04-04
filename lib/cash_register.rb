901c0c525716eedf00baeb04b72e59caccd6aaae
Jorge Ramirez <joramirez96@gmail.com>
2020-04-04 19:15:44 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -18,14 +18,12 @@ class CashRegister
   end
 
   def apply_discount
-    if @discount > 0
-      @to_take_off = (price * discount)/100
-      @total -= @to_take_off
-      return "After the discount, the total comes to $#{total}."
-    else
+    if @discount == 0
       return "There is no discount to apply."
     end
-  end
+    @total -= @total * @discount / 100
+    return "After the dsicount, the total to $#{@total}."
+  end 
 
   def void_last_transaction
     @total -= @items
